196189280f2dbb64d706400143120281e15d47f2
Ygnas <Ygnas@outlook.com>
2023-05-09 22:48:44 +0100

    user navigation

---
+++ src/lib/MainNavigator.svelte
@@ -0,0 +1,11 @@
+<div class="box has-text-centered columns m-2 is-half">
+	<a href="/dashboard" class="column">
+		<i class="fas fa-hand-holding-usd fa-3x" style="color:rgb(153, 196, 74)" />
+	</a>
+	<a href="/account" class="column">
+		<i class="fas fa-user fa-3x" style="color:rgb(156, 70, 28)" />
+	</a>
+	<a href="/logout" class="column">
+		<i class="fas fa-sign-out-alt fa-3x" style="color:rgb(156, 70, 128)" />
+	</a>
+</div>
