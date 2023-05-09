0df05c92805581d758ff52abe80740c4fa9e463f
Ygnas <Ygnas@outlook.com>
2023-05-09 23:17:23 +0100

    new link to navigator

--- src/lib/MainNavigator.svelte
+++ src/lib/MainNavigator.svelte
@@ -2,6 +2,9 @@
 	<a href="/dashboard" class="column">
 		<i class="fas fa-home fa-3x" style="color:rgb(153, 196, 74)" />
 	</a>
+	<a href="/navigator" class="column">
+		<i class="fas fa-drafting-compass fa-3x" style="color:rgb(50, 50, 255)" />
+	</a>
 	<a href="/account" class="column">
 		<i class="fas fa-user fa-3x" style="color:rgb(156, 70, 28)" />
 	</a>
