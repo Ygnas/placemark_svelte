f55eb88d102759c14b561425e0920d7513ededde
Ygnas <Ygnas@outlook.com>
2023-05-10 16:10:50 +0100

    ability to delete an image

--- src/lib/placemarks/PlacemarkDescription.svelte
+++ src/lib/placemarks/PlacemarkDescription.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
+	import { placemarkService } from "../../services/placemark-service";
 	import type { Placemark } from "../../services/placemark-types";
 
 	export let placemark: Placemark;
+
+	async function deleteImage() {
+		await placemarkService.deleteImage(placemark);
+		placemark.img = "";
+	}
 </script>
 
 <div class="column has-text-centered box">
 	{@html placemark.name}
 </div>
 <section>
-	<img src={placemark.img} alt="" />
+	<div>
+		{#if placemark.img}
+			<img src={placemark.img} alt="placemark" />
+			<button on:click={deleteImage} class="button is-small is-fullwidth">Delete image</button>
+		{/if}
+	</div>
 	{@html placemark.description}
 </section>
