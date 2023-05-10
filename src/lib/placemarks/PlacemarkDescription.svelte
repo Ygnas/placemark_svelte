5c4f65a1ea8279bd15dd2cae1827182a2a750b82
Ygnas <Ygnas@outlook.com>
2023-05-10 14:34:09 +0100

    placemark description for navigator

---
+++ src/lib/placemarks/PlacemarkDescription.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Placemark } from "../../services/placemark-types";
+
+	export let placemark: Placemark;
+</script>
+
+<div class="column has-text-centered box">
+	{placemark.name}
+</div>
+<section>
+	<img src={placemark.img} alt="" />
+	{placemark.description}
+</section>
