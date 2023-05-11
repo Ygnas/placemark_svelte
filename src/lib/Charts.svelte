89400fcd39bf3ee0cd7e386ebb4a493c8218c58b
Ygnas <Ygnas@outlook.com>
2023-05-11 21:35:35 +0100

    different chartdata for charts

--- src/lib/Charts.svelte
+++ src/lib/Charts.svelte
@@ -2,26 +2,37 @@
 	// @ts-ignore
 	import Chart from "svelte-frappe-charts";
 	import { onMount } from "svelte";
-	import type { ChartData } from "../services/charts";
+	import { type ChartData, ChartDataType } from "../services/charts";
 	import { placemarkService } from "../services/placemark-service";
-	import { generateByCategoryID } from "../services/placemark-utils";
+	import { generateByCategoryID, generateByUser } from "../services/placemark-utils";
 
-	let byCategoryID: ChartData;
+	let chartData: ChartData;
 	export let charType: string = "bar";
+	export let chartDataType: ChartDataType = ChartDataType.ByCategories;
+	export let title: string = "Placemarks in categories";
 
 	onMount(async () => {
 		placemarkService.reload();
 
-		const categoryList = await placemarkService.getCategories();
-		const placemarkList = await placemarkService.getPlacemarks();
-
-		byCategoryID = generateByCategoryID(categoryList, placemarkList);
+		switch (chartDataType) {
+			case ChartDataType.ByCategories:
+				const categoryList = await placemarkService.getCategories();
+				const placemarkList = await placemarkService.getPlacemarks();
+				chartData = generateByCategoryID(categoryList, placemarkList);
+				break;
+			case ChartDataType.ByUsers:
+				const users = await placemarkService.getAllUsers();
+				chartData = generateByUser(users);
+				break;
+			default:
+				break;
+		}
 	});
 </script>
 
 <div class="columns px-1">
 	<div class="column box has-text-centered">
-		<h1 class="title is-4">Placemarks in categories</h1>
-		<Chart data={byCategoryID} type={charType} />
+		<h1 class="title is-4">{title}</h1>
+		<Chart data={chartData} type={charType} />
 	</div>
 </div>
