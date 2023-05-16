8d299a91be546e0fd17ce5e0fa1be23fac546db2
Ygnas <Ygnas@outlook.com>
2023-05-16 15:08:10 +0100

    google authentication

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import Header from "$lib/Header.svelte";
-	import { placemarkService } from "../../services/placemark-service";
 	import WelcomeNavigator from "$lib/WelcomeNavigator.svelte";
 	import LoginForm from "./LoginForm.svelte";
+	import { initialize, signIn, signOut } from "svelte-google-auth/client";
+	import { invalidateAll } from "$app/navigation";
+	import type { PageData } from "../$types";
+	import { placemarkService } from "../../services/placemark-service";
+	import axios from "axios";
+
+	export let data: PageData;
+	initialize(data, invalidateAll);
+
+	async function google() {
+		if (!data.auth.user) {
+			await signIn();
+		}
+		let response = await axios.post("http://localhost:3000/api/users/authenticate/google", data.auth.user);
+		let success = await placemarkService.google(response, data.auth.user);
+		if (success) {
+			goto("/dashboard");
+		}
+	}
 </script>
 
 <Header>
@@ -17,6 +35,7 @@
 	<div class="column">
 		<div class="box">
 			<h1 class="title">Login</h1>
+			<div class="column"><button on:click={() => google()}>Sign In with google</button></div>
 			<LoginForm />
 		</div>
 	</div>
