3dfefee615e25e6504a46f1968d6fce4dfb92561
Ygnas <Ygnas@outlook.com>
2023-05-17 20:46:58 +0100

    now use env

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,16 @@
 import { SvelteGoogleAuthHook } from 'svelte-google-auth/server';
 import type { Handle } from '@sveltejs/kit';
+import { auth_provider_x509_cert_url, auth_uri, client_id, client_secret, project_id, redirect_uris, token_uri } from '$env/static/private';
 
-// Import client credentials from json file
-import client_secret from '../client_secret.json';
-
-const auth = new SvelteGoogleAuthHook(client_secret.web);
+const auth = new SvelteGoogleAuthHook({
+	client_id: client_id,
+	client_secret: client_secret,
+	project_id: project_id,
+	auth_uri: auth_uri,
+  token_uri: token_uri,
+	auth_provider_x509_cert_url: auth_provider_x509_cert_url,
+	redirect_uris: redirect_uris
+});
 
 export const handle: Handle = async ({ event, resolve }) => {
 	return await auth.handleAuth({ event, resolve });
