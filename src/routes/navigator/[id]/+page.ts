9f1e90b65f6ffa74f45a8a4e16ba82829e72a6fd
Ygnas <Ygnas@outlook.com>
2023-05-17 19:31:34 +0100

    now reloads for authentication headers

--- src/routes/navigator/[id]/+page.ts
+++ src/routes/navigator/[id]/+page.ts
@@ -1,17 +1,16 @@
 import { placemarkService } from "../../../services/placemark-service";
 import type { Placemark } from "../../../services/placemark-types";
-import { generateMarkerSpec } from "../../../services/placemark-utils";
 import type { PageLoad } from "./$types";
 
+export const ssr = false;
+
 export const load: PageLoad = async ({ params }) => {
+  placemarkService.reload();
   const placemark: Placemark = await placemarkService.getPlacemark(encodeURI(params.id));
-  const marker = generateMarkerSpec(placemark);
   return {
     placemark,
-    marker,
     markerLayers: {
       title: placemark.name,
-      markerSpecs: [marker]
     }
   }
 }
